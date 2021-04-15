5e88c55c9e71b7a6ad091bd91befc392ecae3d3f
Aneeq12345 <batch16.049@gmail.com>
2021-04-15 13:30:45 +0500

    Added response file in root

--- authentication/views.py
+++ authentication/views.py
@@ -12,7 +12,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from validate_email import validate_email
-from .response import BaseApiView
+from response import BaseApiView
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.core.mail import BadHeaderError, send_mail
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
@@ -21,12 +21,7 @@ from django.utils.encoding import (
     smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError)
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
-from rest_framework import serializers
-from django.http import HttpResponsePermanentRedirect
 import os
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
-from django.core import serializers
 
 
 class RegisterView(generics.CreateAPIView):
@@ -112,8 +107,6 @@ class RequestPasswordResetEmail(generics.GenericAPIView):
             user = User.objects.get(email=email)
             uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
             token = PasswordResetTokenGenerator().make_token(user)
-            current_site = get_current_site(
-                request=request).domain
             email_body = 'Hello, \n Use token below to reset your password  \
              \n token = '+token+'\n uid = '+uidb64
             try:
